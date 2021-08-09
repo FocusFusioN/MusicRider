42edacc835cda269d0ab5d0635829a36fe6a1e9e
The-LimitedEditioN <88644937+The-LimitedEditioN@users.noreply.github.com>
2021-08-09 19:06:44 +0530

    Update __init__.py

--- MusicRider/services/queues/__init__.py
+++ MusicRider/services/queues/__init__.py
@@ -1,7 +1,7 @@
-from DaisyXMusic.services.queues.queues import clear 
-from DaisyXMusic.services.queues.queues import get
-from DaisyXMusic.services.queues.queues import is_empty
-from DaisyXMusic.services.queues.queues import put
-from DaisyXMusic.services.queues.queues import task_done
+from MusicRider.services.queues.queues import clear 
+from MusicRider.services.queues.queues import get
+from MusicRider.services.queues.queues import is_empty
+from MusicRider.services.queues.queues import put
+from MusicRider.services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
