57c4fa5506b5acd3e610a92938120f25ee7b4ed2
The-LimitedEditioN <88644937+The-LimitedEditioN@users.noreply.github.com>
2021-08-09 19:05:30 +0530

    Update __main__.py

--- MusicRider/__main__.py
+++ MusicRider/__main__.py
@@ -1,4 +1,4 @@
-# DaisyXMusic (Telegram bot project)
+# MusicRider (Telegram bot project)
 # Copyright (C) 2021  Inuka Asith & Rojserbest
 
 # This program is free software: you can redistribute it and/or modify
@@ -18,11 +18,11 @@
 import requests
 from pyrogram import Client as Bot
 
-from DaisyXMusic.config import API_HASH
-from DaisyXMusic.config import API_ID
-from DaisyXMusic.config import BG_IMAGE
-from DaisyXMusic.config import BOT_TOKEN
-from DaisyXMusic.services.callsmusic import run
+from MusicRider.config import API_HASH
+from MusicRider.config import API_ID
+from MusicRider.config import BG_IMAGE
+from MusicRider.config import BOT_TOKEN
+from MusicRider.services.callsmusic import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -34,7 +34,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="DaisyXMusic.modules"),
+    plugins=dict(root="MusicRider.modules"),
 )
 
 bot.start()
