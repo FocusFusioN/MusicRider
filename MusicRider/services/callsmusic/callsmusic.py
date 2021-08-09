ea1d4c1b4490f274af40ae61886df030e8b7ca33
The-LimitedEditioN <88644937+The-LimitedEditioN@users.noreply.github.com>
2021-08-09 20:35:39 +0530

    Update callsmusic.py

--- MusicRider/services/callsmusic/callsmusic.py
+++ MusicRider/services/callsmusic/callsmusic.py
@@ -18,8 +18,8 @@ from typing import Dict
 
 from pytgcalls import GroupCallFactory
 
-from DaisyXMusic.services.callsmusic import client
-from DaisyXMusic.services.queues import queues
+from MusicRider.services.callsmusic import client
+from MusicRider.services.queues import queues
 
 
 instances: Dict[int, GroupCallFactory] = {}
