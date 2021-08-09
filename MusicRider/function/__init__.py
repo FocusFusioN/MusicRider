f7d86af312fea390d24ed7a658c24ee72197da36
The-LimitedEditioN <88644937+The-LimitedEditioN@users.noreply.github.com>
2021-08-09 19:51:54 +0530

    Update __init__.py

--- MusicRider/function/__init__.py
+++ MusicRider/function/__init__.py
@@ -1,5 +1,5 @@
-from DaisyXMusic.function.admins import admins
-from DaisyXMusic.function.admins import get
-from DaisyXMusic.function.admins import set
+from MusicRider.function.admins import admins
+from MusicRider.function.admins import get
+from MusicRider.function.admins import set
 
 __all__ = ["set", "get", "admins"]
